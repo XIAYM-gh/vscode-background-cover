c88d9993321dd6df7ceab5fc8988e1fbf224dda9
XIAYM-gh <xiaym123@qq.com>
2025-06-28 22:15:54 +0800

    Reformat code & clean up

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-	entry: [
-        'src/extension.ts',
-        'src/uninstall.ts'
-    ],
+	entry: ['src/extension.ts', 'src/uninstall.ts'],
 	splitting: false,
 	sourcemap: false,
 	clean: true,
 	format: 'cjs',
-    minify: true,
-    outDir: 'out',
+	minify: true,
+	outDir: 'out',
 	external: ['vscode']
 });
